a8efbe99c418cadb6d692da6457e8b9925264413
= <eduardo.plis@hotmail.com>
2021-12-28 16:25:34 -0300

    Init

---
+++ src/main/java/com/donzelitos/elastikuzao/ElastikuzaoApplication.java
@@ -0,0 +1,13 @@
+package com.donzelitos.elastikuzao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastikuzaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElastikuzaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donzelitos/elastikuzao/ElastikuzaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donzelitos.elastikuzao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElastikuzaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
